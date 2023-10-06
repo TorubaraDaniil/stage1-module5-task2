2300cc16f2a1c8581e8c9f39f842d67d76bb4bd4
daniil-torubara <dabarara@bk.ru>
2023-10-07 00:33:34 +0600

    stage 1 module 5 task 2

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> {
+            List<Integer> resultList = new ArrayList<>();
+            for (Integer value : list) {
+                resultList.add(value / divider);
+            }
+            return resultList;
+        };
     }
 }
